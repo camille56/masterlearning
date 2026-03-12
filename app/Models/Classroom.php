cf696860dde2435cb6681beff17539858f9067ed
camille56 <camille.david.dev@proton.me>
2026-03-12 14:57:31 +0100

    feat(base): ajout api-platform (pour voir les routes)

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use ApiPlatform\Metadata\ApiResource;
 use Illuminate\Database\Eloquent\Model;
 
+#[ApiResource]
 class Classroom extends Model
 {
 
     protected $fillable = ['name', 'school_id'];
+
     public function school(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(School::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\SchoolController;
@@ -10,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::apiResource('schools', SchoolController::class);
@@ -27,3 +31,4 @@ Route::get('classes/{schoolclasses}/students', [ClassroomController::class, 'get
 
 //Les cours d'une matière spécifique.
 Route::get('subjects/{subject}/courses', [SubjectController::class, 'getCourses']);
+
